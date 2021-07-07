542ea2fae8553b8f68cdee4f4b2ffb4d92c98715
ErickInformatica <36520780+ErickInformatica@users.noreply.github.com>
2021-07-07 12:09:22 -0600

    changes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = require('./app')
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://root:root@cluster0.5qksf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
   console.log('Se encuentra conectado a la base de datos');
- 
-  app.listen(process.env.PORT || 3000, function () {
+  app.set('port', process.env.PORT || 3000);
+  app.listen(app.get('port'), function () {
     console.log('El servidor esta arrancando en el puerto: 3000');  
   })
 
