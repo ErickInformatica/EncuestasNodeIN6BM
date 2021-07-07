22f5346a8c75444c19a9260d931c90f935e5e04f
ErickInformatica <36520780+ErickInformatica@users.noreply.github.com>
2021-07-07 12:16:07 -0600

    heroku y mongo atlas

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const app = require('./app')
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://root:root@cluster0.5qksf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
   console.log('Se encuentra conectado a la base de datos');
-  app.set('port', process.env.PORT || 3000);
-  app.listen(app.get('port'), function () {
+  app.listen(process.env.PORT || 3000, function () {
     console.log('El servidor esta arrancando en el puerto: 3000');  
   })
 
